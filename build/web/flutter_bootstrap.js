38c86f6770a3079b75db87eea0581a5be25492ed
Jomonsebs <portfoliobuilders.ind@gmail.com>
2024-11-20 10:29:57 +0530

    Initial commit for Flutter web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "471450969"
+    serviceWorkerVersion: "3876526950"
   }
 });
